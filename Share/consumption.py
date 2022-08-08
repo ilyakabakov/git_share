cb20e61b8340de2d2814cca2d8ba4eb707c0db3f
Ilya Kabakov <ikabakov90@list.ru>
2022-08-08 15:54:45 +0700

    wrote header comment :)

--- Share/consumption.py
+++ Share/consumption.py
@@ -1,3 +1,5 @@
+# My first script for calculate the automobile consumption and price per km
+# it important for me, cause  in my car don't have an on-board computer
 class Consumption:
     def __init__(self, fuel_used: float, traveled_dist: float, price: float):
         self.price = price
@@ -11,7 +13,7 @@ class Consumption:
         return self.price * self.consumption() / 100
 
 
-c = Consumption(40, 250, 49.5)
+c = Consumption(40, 250, 49.35)
 
 print(f'consumption is {c.consumption()} km/h')
 print(f'price per 1km: {c.cost()} RUB')
