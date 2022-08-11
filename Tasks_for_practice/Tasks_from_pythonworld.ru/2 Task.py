b0580c574f90e3dc9fa2e3f546385d9764f162ce
Ilya Kabakov <ikabakov90@list.ru>
2022-08-11 22:48:05 +0700

    solving some tasks... :)

---
+++ Tasks_for_practice/Tasks_from_pythonworld.ru/2 Task.py
@@ -0,0 +1,12 @@
+# Високосный год (2)
+# Написать функцию is_year_leap, принимающую 1 аргумент — год, и возвращающую True, если год високосный, и False иначе.
+
+def is_year_leap(year):
+    if year % 4 == 0 or year % 400 == 0 and year % 100 != 0:
+        return True
+    else:
+        return False
+
+
+is_year = is_year_leap(int(input('Enter year to check for leap year:')))
+print(is_year)
