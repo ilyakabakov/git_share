15b66eb89d1c4a0ea3be42d2851d1f3c8e0ac2ea
Ilya Kabakov <ikabakov90@list.ru>
2022-08-25 03:37:11 +0700

    HomeWork_for_Skillfactory.ru_intensive: Password Generator

--- SkillFactory_intensive/HOMEWORK/ilia_kabakov_passgen.py
+++ SkillFactory_intensive/HOMEWORK/ilia_kabakov_passgen.py
@@ -1,15 +1,17 @@
 import random as rnd
 
 
+#   создаем класс для генерации
 class Password:
     def __init__(self, length: int = 10):
         self.spec_symbols: str = '!@#$%^&*()-_+=;:,./?\|`~[]{}'
         self.digits: str = '1234567890'
         self.alphabet: str = 'abcdefghijklmnopqrstuvwxyzABCDEFJHIJKLMNOPQRSTUVWXYZ'
-        self.password:str = ''
+        self.password: str = ''
         self.length = length
 
     def with_digits(self):
+        """Метод для добавления цифр к генерации"""
         choice = str(input('Do You want to use some digits?(y/n)\n'))
         if choice == 'y' or choice == 'yes' or choice == 'Y' or choice == 'YES':
             self.alphabet += self.digits
@@ -20,6 +22,7 @@ class Password:
             raise ValueError(f"Need to answer Yes or No! Your answer: {choice}")
 
     def with_special_symbols(self):
+        """Метод для добавления символов к генерации"""
         choice = str(input('Do You want to use some special symbols?(y/n)\n'))
         if choice == 'y' or choice == 'yes' or choice == 'Y' or choice == 'YES':
             self.alphabet += self.spec_symbols
@@ -30,17 +33,20 @@ class Password:
             raise ValueError(f"Need to answer Yes or No! Your answer: {choice}")
 
     def check_length(self):
+        """Проверка длинны будущего пароля"""
         if 6 <= self.length <= 100:
             return self.length
         else:
             raise ValueError(f'The value can been min 6 and  max 100! Your value: {self.length}')
 
     def password_gen(self):
+        """Собственно сам генератор"""
         for i in range(self.check_length()):
-            self.password += rnd.choice(self.alphabet)
+            self.password += rnd.choice(self.alphabet)  #yield?
         return self.password
 
     def generate(self):
+        """Метод вызовов"""
         if self.check_length():
             self.with_digits()
             self.with_special_symbols()
@@ -50,5 +56,3 @@ class Password:
 
 pg = Password(int(input('Enter length of the password symbols(min:6, max:100):\n')))
 print(pg.generate())
-
-
