2f2d79da6a939c6ba1fdb466d53a261bca9b61d9
Ilya Kabakov <ikabakov90@list.ru>
2022-09-14 10:05:28 +0700

    HomeWork_for_Skillfactory.ru_intensive: Password Generator

--- Share/consumption.py
+++ Share/consumption.py
@@ -1,5 +1,5 @@
 # My first script for calculate the automobile consumption and price per km
-# it important for me, cause  in my car don't have an on-board computer
+# it important for me, because in my car don't have an on-board computer
 class Consumption:
     def __init__(self, fuel_used: float, traveled_dist: float, price: float):
         self.price = price
@@ -13,11 +13,12 @@ class Consumption:
         return self.price * self.consumption() / 100
 
 
-p = float(input('Price per liter: '))
-t_dist = float(input('Traveled distance: '))
-f_used = float(input('Used fuel: '))
-c = Consumption(f_used, t_dist, p)
+if __name__ == '__main__':
+    p = float(input('Price per liter: '))
+    t_dist = float(input('Traveled distance: '))
+    f_used = float(input('Used fuel: '))
+    c = Consumption(f_used, t_dist, p)
 
-print(f'consumption is {c.consumption()} km/h')
-print(f'price per 1km: {c.cost()} RUB')
-print(f"price per 100km:{c.cost() * 100} RUB")
+    print(f'consumption is {c.consumption()} km/h')
+    print(f'price per 1km: {c.cost()} RUB')
+    print(f"price per 100km:{c.cost() * 100} RUB")
