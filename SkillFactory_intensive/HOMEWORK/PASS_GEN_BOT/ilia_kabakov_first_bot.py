f247396815c2c9be7a7782233f926898691d40bd
Ilya Kabakov <ikabakov90@list.ru>
2022-09-22 23:21:28 +0700

    HomeWork_for_Skillfactory.ru_intensive: Password Generator

--- SkillFactory_intensive/HOMEWORK/PASS_GEN_BOT/ilia_kabakov_first_bot.py
+++ SkillFactory_intensive/HOMEWORK/PASS_GEN_BOT/ilia_kabakov_first_bot.py
@@ -18,23 +18,11 @@ async def get_length(message: types.Message):
     try:
         values: int = message.text
         p_gen = Password(int(values))
-        # p_gen.choice1 = message.text
         await message.reply(f'Your new password: {p_gen.generate()}')
-        # await message.reply(f'Do you want add the digits in password?(y/n)')
 
-    except:     #Yes, PEP8 i know it :)
+
+    except Exception:
         await message.reply(f"Exception. Something went wrong.")
-
-
-# @db.message_handler()
-# async def get_choice1(message: types.Message):
-#     try:
-#         text_v: str = message.text
-#         p_gen = Password()
-#         p_gen.with_digits(text_v)
-#         await message.answer("Do you want add the spec symbols in the password?(y/n)")
-#     except TypeError:
-#         await message.reply("Что-то пошло не так")
 
 
 if __name__ == '__main__':
